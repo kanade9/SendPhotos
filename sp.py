de399591f016c90e623bce1959139b737c494651
kzmaro <maronkazu94@gmail.com>
2019-03-29 16:11:11 +0900

    roleback

--- sp.py
+++ sp.py
@@ -1,13 +1,13 @@
 from flask import *
 import PyLineNotify
 
-#あなたのトークンを入力してください
-TOKEN = 'YOURTOKEN'
+# あなたのトークンを入力してください
+TOKEN = 'GkJ8LNmLBcQQRWjnFZnrp6vTPPPBmBmBdQN6tIY4Och'
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
 
+@app.route("/", methods=["GET", "POST"])
 def Send():
     if request.method == "GET":
         return """
@@ -16,12 +16,15 @@ def Send():
             <input name="url"></input>
             </form>"""
     else:
-        adr=str(request.form["url"])
+        adr = str(request.form["url"])
         PyLineNotify.send_photo_with_message(token=TOKEN, message=' ', path=adr)
         return """{}を送信しました。画像のパスを入力してください。
             <form action="/" method="POST">
             <input name="url"></input>
             </form>""".format(str(request.form["url"]))
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="127.0.0.1", port=8080)
+
+#debug=True
